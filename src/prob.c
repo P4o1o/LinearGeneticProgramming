714faf9b4bf410e162e3a3b7473763ec01f30e06
P4o1o <pchecchin.bp@gmail.com>
2025-05-01 13:41:55 +0200

    SSE2/AVX2/AVX512 Mersenne Twister

--- src/prob.c
+++ src/prob.c
@@ -25,44 +25,44 @@ uint32_t random(void){
             #if defined(INCLUDE_AVX512F)
                 for(uint64_t i = 0; i < STATE_SIZE/16; i++){
                     uint64_t idx = i * 16;
-                    __m512i x = _mm512_load_epi32(rand_engine.state + idx);
-                    __m512i x_next = _mm512_load_epi32(rand_engine.state + ((idx + 16) % (STATE_SIZE)));
+                    __m512i x = _mm512_load_si512(rand_engine.state + idx);
+                    __m512i x_next = _mm512_loadu_si512(rand_engine.state + ((idx + 1) % (STATE_SIZE)));
                     __m512i mag = _mm512_and_si512(x, _mm512_set1_epi32(UMASK));
                     mag = _mm512_or_si512(mag, _mm512_and_si512(x_next, _mm512_set1_epi32(LMASK)));
                     __m512i xA = _mm512_srli_epi32(mag, 1);
                     __m512i mask = _mm512_and_si512(mag, _mm512_set1_epi32(1));
                     xA = _mm512_xor_si512(xA, _mm512_and_si512(mask, _mm512_set1_epi32(A)));
-                    __m512i lastpiece =  _mm512_load_epi32(rand_engine.state + ((idx + M/16) % (STATE_SIZE/16)));
+                    __m512i lastpiece =  _mm512_loadu_si512(rand_engine.state + ((idx + M) % (STATE_SIZE)));
                     __m512i result = _mm512_xor_si512(lastpiece, xA);
-                    _mm512_store_epi64(rand_engine.state + idx, result);
+                    _mm512_store_si512(rand_engine.state + idx, result);
                 }
             #elif defined(INCLUDE_AVX2)
                 for (uint64_t i = 0; i < STATE_SIZE/8; i++) {
                     uint64_t idx = i * 8;
                     __m256i x = _mm256_load_si256((const __m256i*)(rand_engine.state + idx));
-                    __m256i x_next = _mm256_load_si256((const __m256i*)(rand_engine.state + ((idx + 8) % (STATE_SIZE))));
+                    __m256i x_next = _mm256_loadu_si256((const __m256i*)(rand_engine.state + ((idx + 1) % (STATE_SIZE))));
                     __m256i mag = _mm256_and_si256(x, _mm256_set1_epi32(UMASK));
                     mag = _mm256_or_si256(mag, _mm256_and_si256(x_next, _mm256_set1_epi32(LMASK)));
                     __m256i xA = _mm256_srli_epi32(mag, 1);
                     __m256i mask = _mm256_and_si256(mag, _mm256_set1_epi32(1));
                     xA = _mm256_xor_si256(xA, _mm256_and_si256(mask, _mm256_set1_epi32(A)));
-                    __m256i lastpiece = _mm256_load_si256((const __m256i*)(rand_engine.state + ((idx + M) % (STATE_SIZE))));
+                    __m256i lastpiece = _mm256_loadu_si256((const __m256i*)(rand_engine.state + ((idx + M) % (STATE_SIZE))));
                     __m256i result = _mm256_xor_si256(lastpiece, xA);
                     _mm256_store_si256((__m256i*)(rand_engine.state + idx), result);
                 }
             #elif defined(INCLUDE_SSE2)
                 for (uint64_t i = 0; i < STATE_SIZE/4; i++) {
                     uint64_t idx = i * 4;
-                    __m128i x = _mm_loadu_si128((__m128i*)(rand_engine.state + idx));
-                    __m128i x_next = _mm_loadu_si128((__m128i*)(rand_engine.state + ((idx + 4) % (STATE_SIZE))));
+                    __m128i x = _mm_load_si128((__m128i*)(rand_engine.state + idx));
+                    __m128i x_next = _mm_loadu_si128((__m128i*)(rand_engine.state + ((idx + 1) % (STATE_SIZE))));
                     __m128i mag = _mm_and_si128(x, _mm_set1_epi32(UMASK));
                     mag = _mm_or_si128(mag, _mm_and_si128(x_next, _mm_set1_epi32(LMASK)));
                     __m128i xA = _mm_srli_epi32(mag, 1);
                     __m128i mask = _mm_and_si128(mag, _mm_set1_epi32(1));
                     xA = _mm_xor_si128(xA, _mm_and_si128(mask, _mm_set1_epi32(A)));
-                    __m128i lastpiece = _mm_load_si128((__m128i*)(rand_engine.state + ((idx + M) % (STATE_SIZE))));
+                    __m128i lastpiece = _mm_loadu_si128((__m128i*)(rand_engine.state + ((idx + M) % (STATE_SIZE))));
                     __m128i result = _mm_xor_si128(lastpiece, xA);
-                    _mm_storeu_si64((__m128i*)(rand_engine.state + idx), result);
+                    _mm_store_si128((__m128i*)(rand_engine.state + idx), result);
                 }
             #else
                 for(uint64_t i = 0; i < STATE_SIZE; i++){
